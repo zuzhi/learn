bda23b41b4a790607aa3bee3c8da6bd896a9b5d4
zuzhi <zuzhi.hu@gmail.com>
2023-12-03 17:13:03 +0800

    import OpenZeppelin Ownable contract

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,26 +1,22 @@
 // contracts/Box.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-// Import Auth from the access-control subdirectory
-import "./access-control/Auth.sol";
+// Import Ownable from the OpenZeppelin Contracts library
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Box {
+// Make Box inherit from the Ownable contract
+contract Box is Ownable {
+    constructor(address initialOwner) Ownable(initialOwner) {}
+
     uint256 private _value;
-    Auth private _auth;
 
     // Emitted when the stored value changes
     event ValueChanged(uint256 value);
 
-    constructor() {
-	_auth = new Auth(msg.sender);
-    }
-
+    // The onlyOwner modifier restricts who can call the store function
     // Stores a new value in the contract
-    function store(uint256 value) public {
-	// Require that the caller is registered as an administrator in Auth
-	require(_auth.isAdministrator(msg.sender), "Unauthorized");
-
+    function store(uint256 value) public onlyOwner {
         _value = value;
         emit ValueChanged(value);
     }
